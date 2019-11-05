1805b5b1cd614edaa78ce52ab2b3fda4b844b8af
Bendezu Vladislav <bendezuvlad@gmail.com>
2019-11-05 20:37:53 +0300

    Add builder functions for main components

--- src/main.py
+++ src/main.py
@@ -1,54 +1,14 @@
 from src.Canvas import Canvas
 from src.IOController import IOController
-from src.compositing.AdditionComposite import AdditionComposite
-from src.compositing.DiscreteFourierTransform import DiscreteFourierTransform
-from src.drawable.Harmonic import Harmonic
-from src.util.common import N, FROM_NUM, TO_NUM
-from src.drawable.Line import Line
-import copy
+from src.util.buider import harmonic, const, rand, add, line
 
 canvas = Canvas()
 io = IOController()
 
-harmonic_11 = Harmonic(title="Harmonic f=11", N=N, A0=100, f0=11, delta_t=0.001)
-harmonic_110 = Harmonic(title="Harmonic f=110", N=N, A0=100, f0=110, delta_t=0.001)
-harmonic_250 = Harmonic(title="Harmonic f=250", N=N, A0=100, f0=250, delta_t=0.001)
-harmonic_510 = Harmonic(title="Harmonic f=510", N=N, A0=100, f0=510, delta_t=0.001)
-dft_11 = DiscreteFourierTransform("DFT 11", harmonic_11)
-dft_110 = DiscreteFourierTransform("DFT 110", harmonic_110)
-dft_250 = DiscreteFourierTransform("DFT 250", harmonic_250)
-dft_510 = DiscreteFourierTransform("DFT 510", harmonic_510)
-
-polyharmonic = AdditionComposite(
-    "Polyharmonic",
-    Harmonic(title="", N=N, A0=25, f0=11, delta_t=0.001),
-    Harmonic(title="", N=N, A0=35, f0=41, delta_t=0.001),
-    Harmonic(title="", N=N, A0=30, f0=141, delta_t=0.001),
-)
-
-def harmonic_shift():
-    graph = Harmonic(title="Harmonic shift", N=N, A0=100, f0=110, delta_t=0.001)
-    graph.shift(offset=100)
-    return graph
-
-def harmonic_spikes():
-    graph = Harmonic(title="Harmonic spikes", N=N, A0=100, f0=110, delta_t=0.001)
-    graph.add_spikes(count=10, size_multiplier=3)
-    return graph
-
-def harmonic_anti_shift():
-    graph = harmonic_shift()
-    graph.anti_shift()
-    return graph
-
-def harmonic_anti_spikes():
-    graph = harmonic_spikes()
-    graph.suppress_spikes(FROM_NUM, TO_NUM)
-    return graph
-
 
 drawables = enumerate([
-    harmonic_shift(), harmonic_spikes(), harmonic_anti_shift(), harmonic_anti_spikes()
+    rand(), harmonic(),
+    const(10), add(line(), harmonic())
 ])
 
 io.clear_dirs()
@@ -59,9 +19,6 @@ for i, drawable in drawables:
 drawables = io.getDrawablesInDir()
 
 for i, drawable in drawables:
-    # drawable.normalize(S=10)
-    # drawable.add_spikes(count=10, size_multiplier=2)
-    # drawable.shift(offset=150, start=200, end=700)
     canvas.add_drawable(drawable)
 figure = canvas.plot()
 
---
+++ src/util/buider.py
@@ -0,0 +1,71 @@
+import copy
+
+from src.compositing.AdditionComposite import AdditionComposite
+from src.compositing.DiscreteFourierTransform import DiscreteFourierTransform
+from src.compositing.autocorrelation import AutoCorrelation
+from src.compositing.crosscorrelation import CrossCorrelation
+from src.drawable.Drawable import Drawable
+from src.util.common import N, FROM_NUM, TO_NUM, DEFAULT_CLONE
+from src.drawable.Harmonic import Harmonic
+from src.drawable.Line import Line
+from src.drawable.Random import Random
+
+# BASE
+
+def line(k=-2, b=10):
+    return Line("Trend", N=N, k=k, b=b)
+
+def rand(start=FROM_NUM, end=TO_NUM):
+    return Random("Random", N=N, fromNum=start, toNum=end)
+
+def const(value):
+    return Line("Const", N=N, k=0, b=value)
+
+def harmonic(amplitude=10, frequency=15, delta_t=0.001):
+    return Harmonic(title="Harmonic", N=N, A0=amplitude, f0=frequency, delta_t=delta_t)
+
+# MODIFICATION
+
+def shift(drawable, offset, clone=DEFAULT_CLONE):
+    copied = copy.deepcopy(drawable) if clone else drawable
+    copied.shift(offset)
+    return copied
+
+def spikes(drawable, count=10, size=3, clone=DEFAULT_CLONE):
+    copied = copy.deepcopy(drawable) if clone else drawable
+    copied.add_spikes(count=count, size_multiplier=size)
+    return copied
+
+def add(*args):
+    list_args = list(args)
+    title = ' + '.join([arg.title for arg in list_args])
+    return AdditionComposite(title, list_args[0], *list_args[1:])
+
+def trunc(drawable, values):
+    return Drawable(drawable.title, N=values, y=drawable.y[:values])
+
+# ANALYSIS
+
+def dft(drawable):
+    return trunc(DiscreteFourierTransform("Spectrum", drawable), values=int(drawable.getN() / 2))
+
+def anti_shift(drawable, y_min=FROM_NUM, y_max=TO_NUM, clone=DEFAULT_CLONE):
+    copied = copy.deepcopy(drawable) if clone else drawable
+    copied.suppress_spikes(y_min, y_max)
+    return copied
+
+def anti_spikes(drawable, clone=DEFAULT_CLONE):
+    copied = copy.deepcopy(drawable) if clone else drawable
+    copied.suppress_spikes()
+    return copied
+
+def anti_trend(drawable, window_size=3, clone=DEFAULT_CLONE):
+    copied = copy.deepcopy(drawable) if clone else drawable
+    copied.anti_trend(window_size)
+    return copied
+
+def auto(drawable):
+    return AutoCorrelation("AutoCorrelation", drawable)
+
+def cross(d1, d2):
+    return CrossCorrelation("CrossCorrelation", d1, d2)
